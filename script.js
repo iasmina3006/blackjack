7704daf9df634d199dabdc9aace4cfdce5ae8915
”Iasmina” <chia1011@h-ka.de>
2025-01-10 14:21:29 +0100

    JS buttons und html angepasst

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const startGameButton = document.getElementById("start-game");
+    const cardContainer = document.getElementById("card-container");
+});
