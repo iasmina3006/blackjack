a5813b0903f1e862daea971ec13d5d575386b479
”Iasmina” <chia1011@h-ka.de>
2025-01-10 16:04:20 +0100

    Werte und Farben der Karten

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     const startGameButton = document.getElementById("start-game");
     const cardContainer = document.getElementById("card-container");
+     //Werte und Farben der Karten
+     const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+     const suits = ["Herz", "Karo", "Pik", "Kreuz"];
 
     function startGame() {
         cardContainer.innerHTML = "";
+
+        //Beispielkarten
         const cards = ["A", "K"];
         cards.forEach((card) => {
             const cardDiv = document.createElement("div");
@@ -16,6 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
             cardContainer.appendChild(cardDiv);
         });
         console.log("Spiel gestartet: Karten wurden hinzugefügt.");
+    }
         startGameButton.addEventListener("click", startGame);
-
+    
 });
