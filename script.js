5bf78b11f875c0716b84aeac42156a317b7beaac
”Iasmina” <chia1011@h-ka.de>
2025-01-10 14:51:58 +0100

    js basics

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
     const startGameButton = document.getElementById("start-game");
     const cardContainer = document.getElementById("card-container");
+
+    function startGame() {
+        cardContainer.innerHTML = "";
+        const cards = ["A", "K"];
+        cards.forEach((card) => {
+            const cardDiv = document.createElement("div");
+            cardDiv.className = "col-6 col-md-3";
+            cardDiv.innerHTML = `
+                <div class="card text-center p-3">
+                    <span class="fs-1">${card}</span>
+                </div>
+            `;
+            cardContainer.appendChild(cardDiv);
+        });
+        console.log("Spiel gestartet: Karten wurden hinzugefügt.");
+        startGameButton.addEventListener("click", startGame);
+
 });
